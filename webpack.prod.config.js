25c38f4f48302caffc8ef1b744d272ed39f654c9
Alberto Mijares <alberto.mijares@edorasware.com>
2015-03-31 20:25:52 +0200

    minify only app.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,6 @@
-var uglifyJsOptions = {mangle: false}, webpack = require('webpack'),
-    webpackProduction = require('./webpack.base.config.js')('vendor-[hash].js', 'app-[hash].js', 'app-[hash].css');
+var webpack = require('webpack'),
+    webpackProduction = require('./webpack.base.config.js')('vendor-[hash].js', 'app-[hash].js', 'app-[hash].css'),
+    uglifyJsOptions = {mangle: false, test: /app.*\.js($|\?)/i};
 
 webpackProduction.storeStatsTo = 'webpackStatistics';
 webpackProduction.plugins.push(new webpack.optimize.UglifyJsPlugin(uglifyJsOptions));
